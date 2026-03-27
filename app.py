ac72e0e82bba44c7c5f62281da715d18afc6f77a
Gustavo Simas <gustavosimassilva@gmail.com>
2026-03-26 23:14:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     page_title="Ecologia do Conhecimento UFSC",
     page_icon="🌌",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 # Estilização customizada (CSS)
